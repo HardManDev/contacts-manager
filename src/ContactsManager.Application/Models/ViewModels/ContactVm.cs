f13247459109bf13bf40bfe9c538d6a93c91c97b
HardManDev <hardman.dev@protonmail.com>
2022-09-14 22:32:05 +0300

    Created base Contact view model

---
+++ src/ContactsManager.Application/Models/ViewModels/ContactVm.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ContactsManager.Application.Interfaces;
+using ContactsManager.Domain.Entities;
+
+namespace ContactsManager.Application.Models.ViewModels;
+
+public class ContactVm : IMapWith<Contact>
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string MobilePhone { get; set; } = null!;
+
+    public string? JobTitle { get; set; }
+
+    public DateTimeOffset BirthDate { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+
+    public void Mapping(Profile profile) =>
+        profile.CreateMap<ContactVm, Contact>();
+}
