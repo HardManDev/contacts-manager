3683a5cbb65304934062397f12a8aa08f7640c96
HardManDev <hardman.dev@protonmail.com>
2022-09-14 22:20:53 +0300

    Created base Contact entity model

---
+++ src/ContactsManager.Domain/Entities/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactsManager.Domain.Entities;
+
+public class Contact
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string MobilePhone { get; set; } = null!;
+
+    public string? JobTitle { get; set; }
+
+    public DateTimeOffset BirthDate { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+}
