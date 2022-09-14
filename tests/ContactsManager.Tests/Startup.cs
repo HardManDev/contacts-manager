a99b800e0b85bf8d72f9298c2556b8fb928fb87f
HardManDev <hardman.dev@protonmail.com>
2022-09-14 23:16:17 +0300

    Created startup logic for unit tests

---
+++ tests/ContactsManager.Tests/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using ContactsManager.Application;
+using ContactsManager.Application.Interfaces;
+using ContactsManager.Application.Mappings;
+using ContactsManager.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactsManager.Tests;
+
+public class Startup
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddApplication();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+        });
+        services.AddScoped<IApplicationDbContext>(provider =>
+            provider.GetRequiredService<ApplicationDbContext>());
+
+        services.AddAutoMapper(config =>
+        {
+            config.AddProfile(
+                new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(
+                new AssemblyMappingProfile(typeof(IApplicationDbContext)
+                    .Assembly));
+        });
+    }
+}
