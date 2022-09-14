109c6fc60024769868671964ac8736b73b853aad
HardManDev <hardman.dev@protonmail.com>
2022-09-14 22:57:23 +0300

    Created ApplicationDbContext

---
+++ src/ContactsManager.Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ContactsManager.Application.Interfaces;
+using ContactsManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsManager.Database;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        // TODO: Bad practice!
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Contact> Contacts { get; set; } = null!;
+}
