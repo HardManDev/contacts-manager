94d58773965b24ea173781e21d7f1bef15d486c4
HardManDev <hardman.dev@protonmail.com>
2022-09-15 01:00:11 +0300

    Created GetContactList MediatR query

---
+++ src/ContactsManager.Application/Requests/Contacts/Queries/GetContactList/GetContactListQuery.cs
@@ -0,0 +1,10 @@
+﻿using ContactsManager.Application.Models.ViewModels;
+using MediatR;
+
+namespace ContactsManager.Application.Requests.Contacts.Queries.GetContactList;
+
+public class GetContactListQuery : IRequest<IList<ContactVm>>
+{
+    public int? Count { get; set; }
+    public int? Offset { get; set; }
+}
---
+++ src/ContactsManager.Application/Requests/Contacts/Queries/GetContactList/GetContactListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using ContactsManager.Application.Interfaces;
+using ContactsManager.Application.Models.ViewModels;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsManager.Application.Requests.Contacts.Queries.GetContactList;
+
+public class GetContactListQueryHandler
+    : BaseHandler, IRequestHandler<GetContactListQuery, IList<ContactVm>>
+{
+    public GetContactListQueryHandler(IMapper mapper,
+        IApplicationDbContext dbContext)
+        : base(mapper, dbContext)
+    {
+    }
+
+    public async Task<IList<ContactVm>> Handle(GetContactListQuery request,
+        CancellationToken cancellationToken)
+    {
+        var contacts =
+            await DbContext.Contacts
+                .Skip(request.Offset ?? 0)
+                .Take(request.Count ?? 100)
+                .ToListAsync(cancellationToken);
+
+        return Mapper.Map<IList<ContactVm>>(contacts);
+    }
+}
