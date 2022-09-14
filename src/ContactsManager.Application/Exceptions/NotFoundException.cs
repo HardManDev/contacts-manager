37c30247e8b775a8b0dc7eb947335c28cd09b780
HardManDev <hardman.dev@protonmail.com>
2022-09-14 22:33:45 +0300

    Created NotFoundException

---
+++ src/ContactsManager.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactsManager.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string entityName, object key)
+        : base($"Entity \"{entityName}\" with key \"{key}\" not found.")
+    {
+    }
+}
