5570122d16f3f86b1ca6afb9ca176720c7e577bc
HardManDev <hardman.dev@protonmail.com>
2022-09-14 22:25:19 +0300

    Created IApplicationDbContext

---
+++ src/ContactsManager.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ContactsManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsManager.Application.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<Contact> Contacts { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
