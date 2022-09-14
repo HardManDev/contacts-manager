d102e07bc25ccf2ceb35e30ae1d5b08bde50fa6e
HardManDev <hardman.dev@protonmail.com>
2022-09-14 23:03:30 +0300

    Created dependency injection logic for ContactsManager.Database.csproj

---
+++ src/ContactsManager.Database/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using ContactsManager.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactsManager.Database;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDatabase(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlite(configuration["DbConnectionUrl"]);
+        });
+        services.AddScoped<IApplicationDbContext>(provider =>
+            provider.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+}
