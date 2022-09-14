3429f9c9d0ba3a389462fc099cc02e8e3d68b440
HardManDev <hardman.dev@protonmail.com>
2022-09-14 22:30:14 +0300

    Created assembly mapping profile

---
+++ src/ContactsManager.Application/Interfaces/IMapWith.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace ContactsManager.Application.Interfaces;
+
+public interface IMapWith<TSource>
+{
+    void Mapping(Profile profile) =>
+        profile.CreateMap(typeof(TSource), GetType());
+}
